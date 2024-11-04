a1be6be157120ceb793f1e85c1fa24a8aef6f0d1
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2024-11-04 17:26:15 -0300

    profile.tsx configuration

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,47 @@
-import {View, Text} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Image, Platform } from 'react-native';
 
-export default function profile() {
-    return (
-        <View>
-            <Text>Bem vindo sigma da bahia</Text>
-        </View>
-    )
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import {useState} from "react";
+
+export default function TabThreeScreen() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={
+        isLoggedIn?(
+            <Image source={{uri:''}} style={styles.headerImage}
+            ):(
+        <Ionicons size={310} name="happy-outline" style={styles.headerImage} />
+        )}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Profile</ThemedText>
+      </ThemedView>
+      <ThemedText>Aqui é o seu perfil.</ThemedText>
+      <ThemedText>Faça login para obter mais funções.</ThemedText>
+
+
+
+
+    </ParallaxScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
