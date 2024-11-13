f205040ee5187f3c6ebe41a4610fdce2d1be2fd9
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2024-11-13 17:29:32 -0300

    portfoio detail

--- app/(tabs)/portfolios.tsx
+++ app/(tabs)/portfolios.tsx
@@ -7,31 +7,46 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import {useAuth} from "@/context/AuthContext";
+import {useState} from "react";
 
 export default function PortfoliosScreen() {
   const { token,userPortfolios, fetchUserPortfolios} = useAuth();
-
+  const [currentPortfolio, setCurrentPortfolio] = useState<any|null>(null);
   const handleFetchUserPortfolios = async () => {
     await fetchUserPortfolios(token);
+
+  }
+  const handleSetPortfolio = () =>{
+    setCurrentPortfolio({title:'teste'});
+  }
+  if(currentPortfolio){
+    return (
+        <ThemedView>
+          <ThemedText>
+            {currentPortfolio.title}
+          </ThemedText>
+        </ThemedView>
+    )
 
   }
 
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      headerImage={<Ionicons size={310} name="briefcase" style={styles.headerImage} />}>
         <ThemedText type="title"> Seus Portfolios</ThemedText>
-      {userPortfolios?(
-          <ThemedView>
-            <Button title='Recarregar porfolios' onPress={handleFetchUserPortfolios}/>
-            <ThemedText type="subtitle"> Seus Portfolios</ThemedText>
-            {userPortfolios?.map((portfolio,index)=>(
-                <ThemedText key={index}>{portfolio.title}</ThemedText>
-            ))}
-          </ThemedView>
-      ):(
-          <ThemedText type="subtitle"> Faça login ou cadastre-se para ver seus portfolios</ThemedText>
-      )}
+          {userPortfolios?(
+            <ThemedView>
+              <Button title='Recarregar porfolios' onPress={handleFetchUserPortfolios}/>
+              <Button title='Definir portfolio(debug)' onPress={handleSetPortfolio}/>
+              <ThemedText type="subtitle"> Seus Portfolios</ThemedText>
+                {userPortfolios?.map((portfolio,index)=>(
+                   <ThemedText key={index}>{portfolio.title}</ThemedText>
+                 ))}
+              </ThemedView>
+               ):(
+            <ThemedText type="subtitle"> Faça login ou cadastre-se para ver seus portfolios</ThemedText>
+          )}
 
 
     </ParallaxScrollView>
