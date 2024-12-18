b556e4fc449ef695667cfd0e4d7c1c9b98838715
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2024-12-18 17:31:27 -0300

    StockCard.tsx

--- components/StockCard.tsx
+++ components/StockCard.tsx
@@ -6,13 +6,20 @@ import {ThemedText} from "@/components/ThemedText";
 
 
 
-export default function StockCard({thisStock, onPress}:any) {
-    const [logoUrl, setLogoUrl] = useState<string>('');
-
+export default function StockCard({thisStock, onPress, isSelected}:any) {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [quantity, setQuantity] = useState(0);
+    const [value, setValue] = useState(0);
+    const [time, setTime] = useState(0);
+    const handleCardPress = ()=>{
+        setIsExpanded(prev => !prev);
+        if (onPress){onPress()}
+    }
 
     return(
+    <View style={styles.card}>
+    <TouchableOpacity style={styles.cardContent} onPress={handleCardPress}>
 
-        <TouchableOpacity style={styles.card} onPress={onPress}>
             <View style={styles.basicInfo}>
                 {thisStock&&
                 thisStock.stock&&
@@ -44,28 +51,62 @@ export default function StockCard({thisStock, onPress}:any) {
             </View>
 
         </TouchableOpacity>
-    )
+        {isExpanded&&(
+                <View style={styles.cardActions}>
+                    <ThemedText style={styles.infoText}> Movimentar cotas  </ThemedText>
+                    <View>
 
-}
+                    </View>
+                </View>
+                )}
+    </View>
+
+)}
 
  const styles = StyleSheet.create({
-    card:{
+     card:{
+       flexDirection:'column',
+         width: '100%',
+         alignItems:'center',
+         justifyContent:'center',
+
+     },
+    cardContent:{
         height: 150,
         width:'100%',
         borderRadius:10,
         flexDirection:'row',
+        backgroundColor:'#555555',
         alignItems:'center',
-        backgroundColor:'white',
+
         justifyContent:'space-evenly',
 
+
     },
+        cardActions:{
+            flexDirection:'row',
+            width:'80%',
+            height:100,
+            marginTop:-10 ,
+            paddingTop:20,
+            alignItems:"center",
+            backgroundColor:'#555555',
+            borderRadius:10,
+            zIndex:-1
+     },
+     cardActionButtonHolder:{
+
+     },
+     cardActionButton:{
+
+     },
      basicInfo:{
         flexDirection:'column',
          alignItems:'flex-start',
          justifyContent:'center',
          paddingVertical:'2%',
          paddingHorizontal:'2%',
-         width:'15%',
+         width:'20%',
          height:'90%',
          borderRadius:10,
 
@@ -91,5 +132,6 @@ export default function StockCard({thisStock, onPress}:any) {
       logoImg:{
         width:'100%',
         height:'100%',
-    }
+    },
+
  })
